9446985b1cd0407932378593f4e0292ed8c9fab5
jdiejim <jdiejim@gmail.com>
2017-09-10 17:21:28 -0600

    Update PokeCell.js

--- src/components/PokeCell.js
+++ src/components/PokeCell.js
@@ -2,11 +2,11 @@ import React from 'react';
 import sprites from '../assets/sprites.png';
 import './styles/PokeCell.css';
 
-const PokeCell = ({ pokeClass, handleOnClick }) => {
+const PokeCell = ({ pokeClass }) => {
   const { id, backgroundPosition } = pokeClass;
   const style = { backgroundImage: `url(${sprites})`, backgroundPosition };
 
-  return <button onClick={() => handleOnClick(id)} style={style} className="poke-cell"></button>
+  return <button style={style} className="poke-cell"></button>
 };
 
 export default PokeCell;
